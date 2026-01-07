a6101aa18ef6f29a8f42ee3f002fa80c24afc678
quirkyharsh <harshpatil3.1428@gmail.com>
2026-01-07 21:36:19 +0530

    missing element gg

---
+++ ARRAY/missing_element.py
@@ -0,0 +1,16 @@
+"""
+ You are given an array arr[] of size n - 1 that contains distinct integers in the range
+ from 1 to n (inclusive). This array represents a permutation of the integers 
+ from 1 to n with one element missing. Your task is to identify and 
+ return the missing element.
+"""
+class Solution:
+    def missingNum(self, arr):
+        n = len(arr) + 1
+        
+        expected_sum = n * (n + 1) // 2
+
+        actual_sum = sum(arr)
+        
+
+        return expected_sum - actual_sum
