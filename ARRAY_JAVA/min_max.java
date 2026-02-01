33c080c21665b148fd50e293474d95cd23213b37
quirkyharsh <harshpatil3.1428@gmail.com>
2026-02-01 21:34:24 +0530

    2 java array gg

---
+++ ARRAY_JAVA/min_max.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+        int min = arr[0];
+        int max = arr[0];
+        
+        for(int i = 0;i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(min);
+        result.add(max);
+        
+        return result;
+    }
+}
