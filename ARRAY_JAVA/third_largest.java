9f623af8cd21b6a8eb435463df2c4876ca5d840c
quirkyharsh <harshpatil3.1428@gmail.com>
2026-02-06 21:39:55 +0530

    1 java gg

---
+++ ARRAY_JAVA/third_largest.java
@@ -0,0 +1,30 @@
+class Solution {
+    int thirdLargest(int arr[]) {
+
+        if (arr.length < 3) {
+            return -1;
+        }
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > first) {
+                third = second;
+                second = first;
+                first = arr[i];
+            }
+            else if (arr[i] > second) {
+                third = second;
+                second = arr[i];
+            }
+            else if (arr[i] > third) {
+                third = arr[i];
+            }
+        }
+
+        return third;
+    }
+}
