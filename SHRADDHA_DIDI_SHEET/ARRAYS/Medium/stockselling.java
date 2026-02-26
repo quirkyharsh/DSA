19bb3aa048e6006d8bc5df6b944bd67d5cdb9b4d
quirkyharsh <harshpatil3.1428@gmail.com>
2026-02-26 19:24:11 +0530

    2 java array

---
+++ SHRADDHA_DIDI_SHEET/ARRAYS/Medium/stockselling.java
@@ -0,0 +1,26 @@
+package SHRADDHA_DIDI_SHEET.ARRAYS.Medium;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            
+            // Update minimum price
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            
+            // Calculate profit if sold today
+            int profit = prices[i] - minPrice;
+            
+            // Update maximum profit
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
