1153b047daa89cd6a51b50228ff1f78d498aa834
quirkyharsh <harshpatil3.1428@gmail.com>
2026-07-18 11:48:21 +0530

    1 java

---
+++ NEWSTART/ARRAY/5.majorityElement.java
@@ -0,0 +1,22 @@
+class Solution {
+    int majorityElement(int arr[]) {
+        // code here
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            
+            for(int j = 0; j < n; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            
+            if(count > n / 2){
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+}
