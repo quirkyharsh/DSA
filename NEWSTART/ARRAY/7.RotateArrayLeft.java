b0bfd778528880b5ed12a7356a7720e15b59fc78
quirkyharsh <harshpatil3.1428@gmail.com>
2026-07-20 20:53:55 +0530

    2 java

---
+++ NEWSTART/ARRAY/7.RotateArrayLeft.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public void rotateArr(int arr[], int d) {
+        // code here
+        int n = arr.length;
+        
+
+        for (int i = 0; i < d; i++) {
+            int first = arr[0];
+
+          
+            for (int j = 0; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+
+           
+            arr[n - 1] = first;
+        }
+    }
+}
