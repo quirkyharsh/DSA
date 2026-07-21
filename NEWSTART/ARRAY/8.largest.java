567857e0cb2dc2d6f9b31a6253c95742b7477db7
quirkyharsh <harshpatil3.1428@gmail.com>
2026-07-21 18:34:03 +0530

    1 java

---
+++ NEWSTART/ARRAY/8.largest.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+        int max = 0;
+        
+        for(int i = 0;i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        
+        return max;
+    }
+}
