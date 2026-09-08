e572d2bd82dcf9050778a9fe16feceb4963e7191
quirkyharsh <harshpatil3.1428@gmail.com>
2026-09-08 19:50:24 +0530

    1 java

---
+++ ARRAY_JAVA/PalindromeArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static boolean isPalindrome(int[] arr) {
+        // code here
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left <= right){
+            if(arr[left] != arr[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
